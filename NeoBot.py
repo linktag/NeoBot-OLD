3dd8ff44032534593948e0581f6b50093eb4d20e
linktag <linktag@hotmail.fr>
2016-01-29 22:40:17 +0100

    First realease

--- NeoBot.py
+++ NeoBot.py
@@ -8,7 +8,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-
+ 
 #Definition des procédures et fonctions
 
 #Cette donction s'occupe de cliquer avec les informations envoyer
