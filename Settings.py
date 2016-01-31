63ca857c52af736dff6507dca07d1d82ba001f40
linktag <linktag@hotmail.fr>
2016-01-31 12:47:32 +0100

    Add Google Chrome choice in Settings

--- Settings.py
+++ Settings.py
@@ -6,12 +6,17 @@ while x:
     f = open("data",'r')
     file  = (f.read()).split("\n")
     f.close()
-    print ("Just enter the nember of what setting you want to change")
+    print ("\nJust enter the nember of what setting you want to change")
     print ("1) Username :"+file[0])
     print ("2) Password :"+file[1])
     print ("3) Speed :"+file[2]+"s")
     print ("4) See Addprize :"+file[3])
-    print ("5) Make some pause :"+file[4]+"\n")
+    print ("5) Make some pause :"+file[4])
+    #On test le navigateur
+    if file[5] == "1":
+        print ("6) Browser: Google Chrome")
+    else:
+        print ("6) Browser: Firefox")
     print ("0) Stop this programm")
     answer = input ("Your answer:")
     if answer =="0":
@@ -26,8 +31,10 @@ while x:
         file[3] = input("Do you want to see adprize ? (1=Yeah,0=No!):")
     elif answer =="5":
         file[4] = input("Do you want to makes so pause (5% chance between each adds) (1=Yeah,0=No!):")
+    elif answer =="6":
+        file[5] = input("Enter 1 to use Google Chrome or 2 to use Firefox:")
     else:
         print ("Invalid answer.")
     f = open("data",'w')
-    f.write(file[0]+"\n"+file[1]+"\n"+file[2]+"\n"+file[3]+"\n"+file[4])
+    f.write(file[0]+"\n"+file[1]+"\n"+file[2]+"\n"+file[3]+"\n"+file[4]+"\n"+file[5])
     f.close()
